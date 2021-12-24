cc7291db331bb16a5834ec07e23f403f575a21cd
Bureaux Tao <taosh.dev@gmail.com>
2021-12-24 18:18:58 +0800

    add yidu_4k dataset

--- config/config.py
+++ config/config.py
@@ -1,4 +1,8 @@
 # 模型相关的配置
-MAX_SEQ_LEN = 150  # 输入的文本最大长度
-BATCH_SIZE = 64  # 模型训练的BATCH SIZE
-EPOCH = 500  # 模型训练的轮次
+# MAX_SEQ_LEN = 150  # 输入的文本最大长度
+# BATCH_SIZE = 64  # 模型训练的BATCH SIZE
+# EPOCH = 500  # 模型训练的轮次
+
+MAX_SEQ_LEN = 256  # 输入的文本最大长度
+BATCH_SIZE = 32  # 模型训练的BATCH SIZE
+EPOCH = 200  # 模型训练的轮次
--- utils/path.py
+++ utils/path.py
@@ -3,12 +3,20 @@ unlabeled_bio = '../data/unlabeled.txt'
 number_handled_bio = '../data/unlabeled_numbered.txt'
 jsonl = '../data/labdeled.jsonl'
 
-event_type = "pulmonary"
-
-train_file_path = "../data/%s.train" % event_type
-test_file_path = "../data/%s.test" % event_type
+# event_type = "pulmonary"
+#
+# train_file_path = "../data/%s.train" % event_type
+# test_file_path = "../data/%s.test" % event_type
 
 all_path = '../data/all.txt'
+
+# -------------------------------------
+
+event_type = "yidu"
+
+train_file_path = "../data/%s.train" % event_type
+validate_file_path = "../data/%s.validate" % event_type
+test_file_path = "../data/%s.test" % event_type
 
 # BASE_MODEL_DIR = "chinese_L-12_H-768_A-12"
 # BASE_CONFIG_NAME = "bert_config.json"
